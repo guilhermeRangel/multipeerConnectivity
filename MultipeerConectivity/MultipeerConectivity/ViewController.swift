f3647db0b7874c399b28d94d0f7d50316541eb66
Matheus Lima Ferreira <matheuslf.17@gmail.com>
2020-04-26 23:16:08 -0300

    criado a base do projeto

--- MultipeerConectivity/MultipeerConectivity/ViewController.swift
+++ MultipeerConectivity/MultipeerConectivity/ViewController.swift
@@ -8,13 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        title = "P2P - \(UIDevice.current.name)"
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+
+        configureButtons()
     }
 
 
+    func configureButtons() {
+        let myFilesButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(showMyFilesAction))
+        let showDocumentsButton = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(showConnectionPrompt))
+        navigationItem.rightBarButtonItem = showDocumentsButton
+        navigationItem.leftBarButtonItem = myFilesButton
+    }
+    
+    @objc func showMyFilesAction() {
+        
+    }
+    
+    @objc func showConnectionPrompt() {
+        
+    }
 }
 
